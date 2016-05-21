a79905331c92190f8ee08ec292de1d8ab240fc28
Daniel Carmichael <daniel.carmichael1@gmail.com>
2016-05-21 15:22:13 -0400

    added mongo options

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var bpjson = app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-mongoose.connect(config.db);
+mongoose.connect(config.db, config.options);
 var db = mongoose.connection;
 db.on('error', function () {
   throw new Error('unable to connect to database at ' + config.db);
