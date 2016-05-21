8f010c59cdfeda599f3d22036c4f36aaf145bdfb
Daniel Carmichael <daniel.carmichael1@gmail.com>
2016-05-21 15:24:27 -0400

    removed mongo connection

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ var bpjson = app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-mongoose.connect(config.db, config.options);
-var db = mongoose.connection;
-db.on('error', function () {
-  throw new Error('unable to connect to database at ' + config.db);
-});
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -31,20 +26,6 @@ models.forEach(function (model) {
 
 app.get('/potato', function(req, res){
   res.send("EMMAN PUES");
-});
-
-app.get('/users', (req,res) => {
-  mongoose.model('User').find(function(err, users){
-    res.send(users);
-  });
-});
-
-app.post('/users', (req,res) => {
-  var p_name = req.body.name;
-  var User = mongoose.model('User');
-  var newUser = new User({name: p_name});
-  newUser.save();
-  res.sendStatus(200);
 });
 
 
